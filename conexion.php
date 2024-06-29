0137d1e570794685abcac59df8ca3f88dce9fcf4
jaredhg@outlook.com <67456711+Andy-Hub79@users.noreply.github.com>
2024-06-29 17:06:54 -0600

    Fix prod

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+// Habilitar el informe de errores en PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "tu_servidor";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$dbname = "tu_base_de_datos";
+
+?>
